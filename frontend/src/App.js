dce1aed3feb8dc09eaa1fe26e377058c37b4e4b2
karthikmurali60 <karthik.murali60@gmail.com>
2023-08-06 11:45:12 -0700

    Editor Page WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import LandingPage from './pages/LandingPage';
 import HomePage from './pages/HomePage';
 import TrashPage from './pages/TrashPage';
 import PrivateRoutes from './utils/PrivateRoutes';
+import EditorPage from './pages/EditorPage';
 import Cookies from 'js-cookie';
 import './App.css';
 import React from 'react';
@@ -21,9 +22,8 @@ function App() {
         <Routes>
           <Route element={<PrivateRoutes/>}>
               <Route path='/home' element={<HomePage/>} />
-          </Route>
-          <Route element={<PrivateRoutes/>}>
               <Route path='/trash' element={<TrashPage/>} />
+              <Route path='/:user/notes/:user_id' element={<EditorPage/>} />
           </Route>
           <Route path='/' element={isLoggedIn() ? <HomePage/> : <LandingPage/>}/>
         </Routes>
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,12 +1,36 @@
-import { Drawer, List, Stack, Toolbar } from "@mui/material";
+import { Drawer, List, Stack, Toolbar, Button, ListItemButton, ListItemIcon } from "@mui/material";
 import React from "react";
 import SidebarItem from "./SidebarItem";
-import { ListItemButton, ListItemIcon } from "@mui/material";
 import { Link } from "react-router-dom";
 import LogoutIcon from "@mui/icons-material/Logout";
+import SendIcon from "@mui/icons-material/Send";
 import Cookies from "js-cookie";
+import { put } from "../lib/Requests";
 
 export default function Sidebar(props) {
+  const user = Cookies.get("username");
+
+  const handleClick = async () => {
+    try {
+      const url = `${process.env.REACT_APP_BACKEND_URL}/${user}/notes`;
+      const payload_data = {
+        title: '',
+        content: '',
+        username: user,
+        tags: [],
+      };
+
+      put(url, payload_data, {
+        success: function (data) {
+          window.location.href = `/${user}/notes/${data['note_id']}`;
+        },
+      });
+      // Handle response
+    } catch (error) {
+      console.error('Error saving content:', error);
+    }
+  };
+
   return (
     <Drawer
       variant="permanent"
@@ -28,6 +52,19 @@ export default function Sidebar(props) {
             {/* <Avatar src={assets.images.logo} /> */}
           </Stack>
         </Toolbar>
+        <Button variant="contained" endIcon={<SendIcon />}
+          component={Link}
+          sx={{
+            color: "#FFFFFF",
+            paddingY: "12px",
+            paddingX: "24px"
+          }}
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          Create Note
+        </Button>
         <SidebarItem
           url="/home"
           name="Home"
@@ -49,10 +86,10 @@ export default function Sidebar(props) {
             paddingX: "24px",
             paddingTop: "205%"
           }}
-          onClick={(event) => {
+          onClick={() => {
             Cookies.remove("username");
             Cookies.remove("name");
-            window.location.href = `/home`;
+            window.location.href = `/`;
           }}
         >
           <ListItemIcon
