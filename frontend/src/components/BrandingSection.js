077f171450113b5844460934f8157e302b39dfb4
Karthik Murali <karthik.murali60@gmail.com>
2023-10-21 18:13:43 +0000

    Add brand screen animation

--- frontend/src/components/BrandingSection.js
+++ frontend/src/components/BrandingSection.js
@@ -1,11 +1,26 @@
 import "./BrandingSection.css";
+import { TypeAnimation } from "react-type-animation";
 import React from "react";
 
 export default function BrandingSection() {
   return (
-    <div className="signup-info">
-      {/* <h1>Welcome to the First Section</h1>
-      <p>This section has a grey background and takes 75% width of the page.</p> */}
+    <div className="signup-info" style={{ marginTop: "20%", marginLeft: "5%"}}>
+      <TypeAnimation
+        sequence={[
+          // Same substring at the start will only be typed once, initially
+          "Welcome to 🗒 EverWrite.",
+          2000,
+          "Capture and organize your thoughts.",
+          1000,
+          "Capture and organize your ideas.",
+          1000,
+          "Capture and organize your information.",
+          1000,
+        ]}
+        speed={50}
+        style={{ fontSize: "3em", color: "white", fontFamily: "sans-serif", fontWeight: "bold", display: 'block' }}
+        repeat={Infinity}
+      />
     </div>
   );
 }
